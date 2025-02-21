6b4c0230248c4ce9b50205a7fffb53d5902123bc
rjpc <ryan@rjpc.net>
2025-02-20 21:14:56 -0800

    add search spinner

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,6 +1,14 @@
-import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
+import {
+  Form,
+  Link,
+  NavLink,
+  Outlet,
+  useNavigation,
+  useSubmit,
+} from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
+import { useEffect } from "react";
 
 export async function loader({
   request,
@@ -8,15 +16,28 @@ export async function loader({
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    return { contacts };
+    return { contacts, q };
 }
   
 export default function SidebarLayout({
   loaderData,
 }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+  const { contacts, q } = loaderData;
 
   const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <>
@@ -25,9 +46,18 @@ export default function SidebarLayout({
           <Link to="about">React Router Contacts</Link>
         </h1>
         <div>
-          <Form id="search-form" role="search">
+          <Form id="search-form" role="search"
+                onChange={(event) =>
+                  submit(event.currentTarget)
+                }>
             <input
               aria-label="Search contacts"
+              className={
+                navigation.state === "loading" && !searching
+                  ? "loading"
+                  : ""
+              }
+              defaultValue={q || ""}
               id="q"
               name="q"
               placeholder="Search"
@@ -35,7 +65,7 @@ export default function SidebarLayout({
             />
             <div
               aria-hidden
-              hidden={true}
+              hidden={!searching}
               id="search-spinner"
             />
           </Form>
